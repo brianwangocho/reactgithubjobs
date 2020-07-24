e28764715e637784eca927235ec0f52499fd0967
brianwangocho <bwangocho@gmail.com>
2020-07-24 11:56:05 +0300

    Updates

---
+++ precache-manifest.9ffe8cae9f89e6639e9f50be253b7fd1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4953b7e7c7a31599126b4d80ebdf746a",
+    "url": "/git%20remote%20add%20origin%20https://github.com/brianwangocho/reactgithubjobs/index.html"
+  },
+  {
+    "revision": "246500b7fe46e506d501",
+    "url": "/git%20remote%20add%20origin%20https://github.com/brianwangocho/reactgithubjobs/static/css/main.62969d29.chunk.css"
+  },
+  {
+    "revision": "8f43306b3d58a2bb5e13",
+    "url": "/git%20remote%20add%20origin%20https://github.com/brianwangocho/reactgithubjobs/static/js/2.99a24d3f.chunk.js"
+  },
+  {
+    "revision": "8ebb70227fec3b61ad3af3d99ebe8940",
+    "url": "/git%20remote%20add%20origin%20https://github.com/brianwangocho/reactgithubjobs/static/js/2.99a24d3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "246500b7fe46e506d501",
+    "url": "/git%20remote%20add%20origin%20https://github.com/brianwangocho/reactgithubjobs/static/js/main.87ae3e0e.chunk.js"
+  },
+  {
+    "revision": "b68ced1ed485423e2f17",
+    "url": "/git%20remote%20add%20origin%20https://github.com/brianwangocho/reactgithubjobs/static/js/runtime-main.41cf800c.js"
+  }
+]);
